45c48705534767f4bcea268b2129fe94854063f6
IanScottWilson <ianscottwilson@yahoo.co.uk>
2012-10-12 14:18:43 +0100

    initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SVGStoreV100::Application.config.secret_token = 'f9b1205ae15cfae07fb9aa0d06bcdb80dfeba8be0834f794602ced0601598306c0ea79649be91b7f9bfd09746f73fa1e47ae3ff1bcc3017487ceb395411dca95'
